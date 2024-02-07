188cfe3c9fb1cb2c9af34779ddd97805ab04b874
Jaap <j.v.krugten@gmail.com>
2024-02-07 09:22:57 +0100

    commit LA

--- stytra/examples/closed_loop_exp.py
+++ stytra/examples/closed_loop_exp.py
@@ -1,8 +1,6 @@
 import numpy as np
 import pandas as pd
 from pathlib import Path
-import socket
-import json
 
 from stytra import Stytra
 from stytra.stimulation import Protocol
@@ -10,6 +8,7 @@ from stytra.stimulation.stimuli import Basic_CL_1D, GratingStimulus
 from lightparam import Param
 
 REQUIRES_EXTERNAL_HARDWARE = False
+
 
 class ClosedLoop1DProt(Protocol):
     name = "closed_loop1D_gratings"
@@ -60,15 +59,6 @@ class ClosedLoop1DProt(Protocol):
         )
         return stimuli
 
-def send_vigor_data(vigor_data):
-    HOST = 'f462i-8840e6'
-    PORT = 12345
-    vigor_data = stytra.experiment.estimator.get_velocity()
-    
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        s.sendall(json.dumps(vigor_data).encode())
 
 if __name__ == "__main__":
     s = Stytra(protocol=ClosedLoop1DProt())
-
